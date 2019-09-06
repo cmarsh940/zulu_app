388528534db96a69ceae8c19b2e2f066beef2c05
Cam <cam.marshall940@yahoo.com>
2019-09-06 16:25:39 -0500

    fixed g signin on android

--- lib/login/widgets/login_form.dart
+++ lib/login/widgets/login_form.dart
@@ -2,8 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_facebook_login/flutter_facebook_login.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+//import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
 import 'package:project_z/auth/authentication.dart';
 import 'package:project_z/data/repositories.dart';
@@ -13,9 +13,9 @@ import 'package:project_z/utils/popUp.dart';
 import '../login.dart';
 import 'login_button.dart';
 
-FacebookLogin _facebookLogin = FacebookLogin();
+//FacebookLogin _facebookLogin = FacebookLogin();
 
-GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['profile', 'email']);
+ GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['profile', 'email']);
 class LoginForm extends StatefulWidget {
   final ClientRepository _clientRepository;
 
@@ -146,22 +146,22 @@ class _LoginFormState extends State<LoginForm> {
                           Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: <Widget>[
-                              IconButton(
-                                icon: Image.asset(
-                                  'assets/icons/loginWithGoogle.png',
-                                ),
-                                iconSize: 60,
-                                onPressed: () {
-                                  _handleGoogleSignIn();
-                                },
-                              ),
+                               IconButton(
+                                 icon: Image.asset(
+                                   'assets/icons/loginWithGoogle.png',
+                                 ),
+                                 iconSize: 60,
+                                 onPressed: () {
+                                   _handleGoogleSignIn();
+                                 },
+                               ),
                               IconButton(
                                 icon: Image.asset(
                                   'assets/icons/loginWithFacebook.png',
                                 ),
                                 iconSize: 60,
                                 onPressed: () {
-                                  _handleFacebookSignIn();
+//                                  _handleFacebookSignIn();
                                 },
                               )
                             ],
@@ -208,77 +208,78 @@ class _LoginFormState extends State<LoginForm> {
     );
   }
 
-  Future<void> _handleGoogleSignIn() async {
-    try {
-      final GoogleSignInAccount googleUser = await _googleSignIn.signIn().catchError((onError) {
-        print("Error $onError");
-      });
-      print('##### GOOGLE ###### $googleUser');
-      if (googleUser == null) {
-        var title = 'Google Signin Error';
-        var message = 'There was a problem signing into goolgle.';
-        showAlertPopup(context, title, message);
-      } else {
-        var email = _googleSignIn.currentUser.email;
-        var password = 'Google' + _googleSignIn.currentUser.id;
-        _loginBloc.dispatch(
-          LoginButtonPressed(
-            email: email,
-            password: password,
-          ),
-        );
-      }
-    } catch (error) {
-      print('google signin error');
-      var title = 'Google Signin Error';
-      var message = error;
-      showAlertPopup(context, title, message);
-    }
-  }
-  Future<void> _handleFacebookSignIn() async {
-    String email;
-    String id;
-    final results = await _facebookLogin.logInWithReadPermissions(['email']);
-    switch (results.status) {
-      case FacebookLoginStatus.loggedIn:
-        final token = results.accessToken.token;
-        final graphResponse = await http.get('https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email&access_token=$token');
-        Map profile = json.decode(graphResponse.body);
-        for (var i = 0; i < profile.length; i++) {
-          var a = profile.entries.elementAt(i);
-          print(a);
-          if (a.key == 'email') {
-            email = a.value;
-          } 
-          else if (a.key == 'id') {
-            id = a.value.toString();
-          }
-        }
-        var password = 'Facebook' + id;
-        _loginBloc.dispatch(
-          LoginButtonPressed(
-            email: email,
-            password: password,
-          ),
-        );
-        break;
-      case FacebookLoginStatus.cancelledByUser :
-        var title = 'Facebook Signin Error';
-        var message = 'There was a problem signing in with Facebook.';
-        showAlertPopup(context, title, message);
-        break;
+   Future<void> _handleGoogleSignIn() async {
+     try {
+       final GoogleSignInAccount googleUser = await _googleSignIn.signIn().catchError((onError) {
+         print("Error $onError");
+       });
+       print('##### GOOGLE ###### $googleUser');
+       if (googleUser == null) {
+         var title = 'Google Signin Error';
+         var message = 'There was a problem signing into goolgle.';
+         showAlertPopup(context, title, message);
+       } else {
+         var email = _googleSignIn.currentUser.email;
+         var password = 'Google' + _googleSignIn.currentUser.id;
+         _loginBloc.dispatch(
+           LoginButtonPressed(
+             email: email,
+             password: password,
+           ),
+         );
+       }
+     } catch (error) {
+       print('google signin error');
+       var title = 'Google Signin Error';
+       var message = error;
+       showAlertPopup(context, title, message);
+     }
+   }
 
-      case FacebookLoginStatus.error :
-        print('FACEBOOK LOGIN ERROR');
-        var title = 'Facebook Signin Error';
-        var message = 'There was a problem signing in with Facebook.';
-        showAlertPopup(context, title, message);
-        setState(() {
-          rebuild += 1;
-        });
-        
-        break;
-    }
-    
-  }
+//  Future<void> _handleFacebookSignIn() async {
+//    String email;
+//    String id;
+//    final results = await _facebookLogin.logInWithReadPermissions(['email']);
+//    switch (results.status) {
+//      case FacebookLoginStatus.loggedIn:
+//        final token = results.accessToken.token;
+//        final graphResponse = await http.get('https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email&access_token=$token');
+//        Map profile = json.decode(graphResponse.body);
+//        for (var i = 0; i < profile.length; i++) {
+//          var a = profile.entries.elementAt(i);
+//          print(a);
+//          if (a.key == 'email') {
+//            email = a.value;
+//          }
+//          else if (a.key == 'id') {
+//            id = a.value.toString();
+//          }
+//        }
+//        var password = 'Facebook' + id;
+//        _loginBloc.dispatch(
+//          LoginButtonPressed(
+//            email: email,
+//            password: password,
+//          ),
+//        );
+//        break;
+//      case FacebookLoginStatus.cancelledByUser :
+//        var title = 'Facebook Signin Error';
+//        var message = 'There was a problem signing in with Facebook.';
+//        showAlertPopup(context, title, message);
+//        break;
+//
+//      case FacebookLoginStatus.error :
+//        print('FACEBOOK LOGIN ERROR');
+//        var title = 'Facebook Signin Error';
+//        var message = 'There was a problem signing in with Facebook.';
+//        showAlertPopup(context, title, message);
+//        setState(() {
+//          rebuild += 1;
+//        });
+//
+//        break;
+//    }
+//
+//  }
 }
