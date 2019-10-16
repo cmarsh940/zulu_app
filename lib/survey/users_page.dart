ed6d357e395560762bdcd7c0dabd751f4aba7195
Cam <cam.marshall940@yahoo.com>
2019-10-16 15:10:12 -0500

    added more on users page

--- lib/survey/users_page.dart
+++ lib/survey/users_page.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-import 'package:contacts_service/contacts_service.dart';
 import 'package:project_z/data/repositories.dart';
 import 'package:project_z/models/survey.dart';
+import 'package:project_z/survey/widgets/add_phone_contact.dart';
 import 'package:project_z/survey/widgets/add_user_dialog.dart';
 import 'package:project_z/survey/widgets/user_list.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class UsersPage extends StatelessWidget {
   final SurveyRepository _surveyRepository;
   final Survey survey;
+  final bool dialVisible = true;
 
   UsersPage({Key key, @required SurveyRepository surveyRepository, this.survey})
       : assert(surveyRepository != null),
         _surveyRepository = surveyRepository,
         super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
@@ -22,142 +24,43 @@ class UsersPage extends StatelessWidget {
       body: Center(
         child: UserList(surveyRepository: _surveyRepository, survey: survey),
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: SpeedDial(
         heroTag: "addUser",
         tooltip: 'Add User',
-        child: Icon(Icons.person_add),
-        onPressed: () => Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) {
-            return AddUserDialog(surveyRepository: _surveyRepository, id: survey.id);
-          }),
-        )
-      ),
-    );
-  }
-}
-
-
-class ItemsTile extends StatelessWidget {
-  ItemsTile(this._title, this._items);
-  final Iterable<Item> _items;
-  final String _title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        ListTile(title: Text(_title)),
-        Column(
-          children: _items.map((i) => Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: ListTile(
-              title: Text(i.label ?? ""),
-              trailing: Text(i.value ?? ""),
-            ),),
-          ).toList(),
-        ),
-      ],
-    );
-  }
-}
-
-class AddContactPage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _AddContactPageState();
-}
-
-class _AddContactPageState extends State<AddContactPage> {
-  Contact contact = Contact();
-  PostalAddress address = PostalAddress(label: "Add User");
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Add a contact"),
-        actions: <Widget>[
-          FlatButton(
-            onPressed: () {
-              _formKey.currentState.save();
-              contact.postalAddresses = [address];
-              ContactsService.addContact(contact);
-              Navigator.of(context).pop();
-            },
-            child: Icon(Icons.save, color: Colors.white),
-          )
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.all(12.0),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: <Widget>[
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'First name'),
-                onSaved: (v) => contact.givenName = v,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Middle name'),
-                onSaved: (v) => contact.middleName = v,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Last name'),
-                onSaved: (v) => contact.familyName = v,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Prefix'),
-                onSaved: (v) => contact.prefix = v,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Suffix'),
-                onSaved: (v) => contact.suffix = v,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Phone'),
-                onSaved: (v) => contact.phones = [Item(label: "mobile", value: v)],
-                keyboardType: TextInputType.phone,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'E-mail'),
-                onSaved: (v) => contact.emails = [Item(label: "work", value: v)],
-                keyboardType: TextInputType.emailAddress,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Company'),
-                onSaved: (v) => contact.company = v,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Job'),
-                onSaved: (v) => contact.jobTitle = v,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Street'),
-                onSaved: (v) => address.street = v,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'City'),
-                onSaved: (v) => address.city = v,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Region'),
-                onSaved: (v) => address.region = v,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Postal code'),
-                onSaved: (v) => address.postcode = v,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'Country'),
-                onSaved: (v) => address.country = v,
-              ),
-            ],
+        animatedIcon: AnimatedIcons.menu_close,
+        animatedIconTheme: IconThemeData(size: 22.0),
+        // child: Icon(Icons.add),
+        onOpen: () => print('OPENING DIAL'),
+        onClose: () => print('DIAL CLOSED'),
+        visible: dialVisible,
+        curve: Curves.bounceIn,
+        children: [
+          SpeedDialChild(
+            label: 'New User',
+            child: Icon(Icons.person_add, color: Colors.white),
+            backgroundColor: Colors.greenAccent,
+            onTap: () => Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) {
+                return AddUserDialog(surveyRepository: _surveyRepository, id: survey.id);
+              }),
+            ),
+            labelStyle: TextStyle(fontWeight: FontWeight.w500),
+            labelBackgroundColor: Colors.greenAccent,
           ),
-        ),
-      ),
+          SpeedDialChild(
+            child: Icon(Icons.contacts, color: Colors.white),
+            backgroundColor: Colors.blueAccent,
+            onTap: () => Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) {
+                return AddPhoneContact();
+              }),
+            ),
+            label: 'Add Contact',
+            labelStyle: TextStyle(fontWeight: FontWeight.w500),
+            labelBackgroundColor: Colors.blueAccent,
+          ),
+        ],
+      )
     );
   }
 }
-
